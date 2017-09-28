3f1845a3875f68b44cfdb2894cf490255d0c143e
deathofasellout <javascriptforlife@gmail.com>
2017-09-28 16:37:26 -0700

    forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
   def index
-    @users =  User.all
+
   end
-  
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      fail
+    end 
+  end
+
+  def new
+    @user = User.new
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email,:name,:password,:birthday)
+  end
 end
