1710558ce245f7d23350c71104f72f775613b474
BryanReese <bryan_reese209@yahoo.com>
2017-09-29 14:32:05 -0700

    logout creted

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,5 @@
 module SessionsHelper
-	
+
 
 	def login(user)
 		session[:user_id] = user.id
@@ -8,6 +8,11 @@ module SessionsHelper
 
 	def current_user
 		@current_user ||= User.find_by_id(session[:user_id])
+	end 
+
+	def logout
+		@current_user = session[:id] = nil
+		redirect_to root_path
 	end 
 
 end
