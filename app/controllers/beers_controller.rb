9fb6be0378535e68a8e154d76693f1702a9ac266
deathofasellout <javascriptforlife@gmail.com>
2017-10-03 11:17:23 -0700

    search bar

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,23 +1,26 @@
 class BeersController < ApplicationController
 
-
+	def index
+	  @q = Beer.ransack(params[:q])
+	  @beers = @q.result(distinct: true)
+  end
 	def create
 	  @beer = Beer.new(validate_beer)
 	  if @beer.save
 		  redirect_to '/beers'
-	  end 
-	end 
+	  end
+	end
 
-	def index
-		@beers = Beer.all
-	end 
+	# def index
+	# 	@beers = Beer.all
+	# end
 
 
-	private 
+	private
 
 	def validate_beer
     	params.require(:beer).permit(:name, :style, :brewery, :country, :ibu, :abv, :image)
-  end 
+  end
 
 
 end
